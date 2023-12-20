723dd192f05417092f1b86334df6eed44e427a4d
Angelo Salamanca <angelosteevensm@ufps.edu.co>
2023-12-20 11:06:33 -0500

    Implementando Swagger

--- src/main/java/com/examen/ExamenApplication.java
+++ src/main/java/com/examen/ExamenApplication.java
@@ -3,7 +3,10 @@ package com.examen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
 @SpringBootApplication
+@EnableOpenApi
 public class ExamenApplication {
 
 	public static void main(String[] args) {
